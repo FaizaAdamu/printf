ffa111f364742577af06b8d4d4246f924372cc19
queenbukkie <akinwaleliz00@gmail.com>
2023-05-28 05:49:59 -0700

    updated

--- handle_print.c
+++ handle_print.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * handle_print - Prints all the arguements based on its type
- * @fmt: Formatted the string.
- * @list: List the arguements for printing.
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
  * @ind: ind.
  * @buffer: Buffer array to handle print.
- * @flags: Calculates the flags that are active
+ * @flags: Calculates active flags
  * @width: get width.
- * @precision : calculates the precisions that are active
+ * @precision: Precision specification
  * @size: Size specifier
  * Return: 1 or 2;
  */
@@ -19,8 +19,7 @@ int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 		{'c', print_char}, {'s', print_string}, {'%', print_percent},
 		{'i', print_int}, {'d', print_int}, {'b', print_binary},
 		{'u', print_unsigned}, {'o', print_octal}, {'x', print_hexadecimal},
-		{'X', print_hexa_upper}, {'p', print_pointer}, {'S', print_non_printable},
-		{'r', print_reverse}, {'R', print_rot13string}, {'\0', NULL}
+		{'X', print_hexa_upper}, {'\0', NULL}
 	};
 	for (i = 0; fmt_types[i].fmt != '\0'; i++)
 		if (fmt[*ind] == fmt_types[i].fmt)
